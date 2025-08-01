6185bf0906b70438e23d0dc4caabe7e50e2a11c8
MauricioMorales123 <mauriciomorales1101@gmail.com>
2025-08-01 09:08:45 -0500

    cambio de comentario en BienvenidaController.java

--- BienvenidaController.java
+++ BienvenidaController.java
@@ -1,7 +1,3 @@
-/**
- * 
- * se utilizo spring initializr para crear el proyecto con spring boot
- */
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -15,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author cmmau
  */
+/**
+ *   
+ * se crea un controlador que no de una bienvenida
+ */   
 @RestController
 public class BienvenidaController {
     @GetMapping("/bienvenido")
