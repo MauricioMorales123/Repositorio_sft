3408632fd2a27e0cd8d6cd2d8013771e0ebba18a
Mauricio Morales <mauriciomorales1101@gmail.com>
2025-08-01 08:26:03 -0500

    Añado módulo con framework spring

---
+++ BienvenidaController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sft.sftweb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author cmmau
+ */
+@RestController
+public class BienvenidaController {
+    @GetMapping("/bienvenido")
+    public String bienvenido (){
+        return "Bienvenido a SFT";
+    }
+    
+}
---
+++ SftwebApplication.java
@@ -0,0 +1,14 @@
+
+package com.sft.sftweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftwebApplication.class, args);
+	}
+
+}
